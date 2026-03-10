0999cde71fa1ed4587e55dbea552402e8e9b719e
pablomseg <60855555+pablomseg@users.noreply.github.com>
2026-03-10 15:46:40 -0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 15:36:23 2026
+
+@author: Pablo
+"""
+
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Hello Streamlit", page_icon="👋")
+
+st.title("Hola, mundo")
+st.write("Esta es mi primera app en Streamlit.")
+
+nombre = st.text_input("Tu nombre", value="Pablo")
+
+if st.button("Saludar"):
+    st.success(f"Hola, {nombre} 👋")
